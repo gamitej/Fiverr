4382197a705f93d2a9871619134d3fb141861266
gamitej <gamitejpratapsingh998@gmail.com>
2024-03-01 17:31:55 +0530

    REFACTOR : home page

---
+++ client/src/data/home.data.tsx
@@ -0,0 +1,17 @@
+export const featureData = [
+  {
+    img: "./img/check.png",
+    title: "The best for every budget",
+    para: "Find high-quality services at every price porint. No hourly rates,just project-based pricing.",
+  },
+  {
+    img: "./img/check.png",
+    title: "The best for every budget",
+    para: "Find high-quality services at every price porint. No hourly rates,just project-based pricing.",
+  },
+  {
+    img: "./img/check.png",
+    title: "The best for every budget",
+    para: "Find high-quality services at every price porint. No hourly rates,just project-based pricing.",
+  },
+];
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -6,25 +6,8 @@ import TrustedBy from "./trustedBy/TrustedBy";
 import Slide from "@/components/Silder/Slide";
 import CategoryCard from "@/components/CategoryCard/CategoryCard";
 // data
+import { featureData } from "@/data/home.data";
 import { cardsData } from "@/data/catCard.data";
-
-const data = [
-  {
-    img: "./img/check.png",
-    title: "The best for every budget",
-    para: "Find high-quality services at every price porint. No hourly rates,just project-based pricing.",
-  },
-  {
-    img: "./img/check.png",
-    title: "The best for every budget",
-    para: "Find high-quality services at every price porint. No hourly rates,just project-based pricing.",
-  },
-  {
-    img: "./img/check.png",
-    title: "The best for every budget",
-    para: "Find high-quality services at every price porint. No hourly rates,just project-based pricing.",
-  },
-];
 
 const Home = () => {
   return (
@@ -36,11 +19,12 @@ const Home = () => {
           <CategoryCard item={item} key={`card-cat-${idx}`} />
         ))}
       </Slide>
+      {/* ========================== FEATURES ======================== */}
       <div className="features">
         <div className="container">
           <div className="item">
             <h1>A whole world of freelance talent at your fingertips</h1>
-            {data.map((item, idx) => (
+            {featureData.map((item, idx) => (
               <React.Fragment key={idx}>
                 <div className="title">
                   <img src={item.img} alt="" />
@@ -59,6 +43,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      {/* ========================== EXPLORE ========================= */}
       <div className="explore">
         <div className="container">
           <h1>Explore the marketplace</h1>
@@ -147,6 +132,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      {/* ======================== FEATURES DARK ====================== */}
       <div className="features dark">
         <div className="container">
           <div className="item">
