5c9b0b63d8f23ead91a3eb4112b23011262fc55d
gamitej <gamitejpratapsingh998@gmail.com>
2024-03-01 10:22:05 +0530

    FIX : @ path import issue resolved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
