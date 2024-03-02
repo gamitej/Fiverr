0a929d3cde630fad5ce276ba51abfb0fe7c6eb5d
gamitej <gamitejpratapsingh998@gmail.com>
2024-03-02 16:57:54 +0530

    NEW : gigs added

--- client/src/pages/gigs/Gigs.tsx
+++ client/src/pages/gigs/Gigs.tsx
@@ -1,5 +1,63 @@
-const Gigs = () => {
-  return <div>Gigs</div>;
-};
+import "./Gigs.scss";
+import { useState } from "react";
+// components
+import GigCard from "@/components/gigCard/GigCard";
+// data
+import { gigsData } from "@/data/catCard.data";
+
+function Gigs() {
+  const [sort, setSort] = useState("sales");
+  const [open, setOpen] = useState(false);
+
+  const reSort = (type: string) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  /**
+   * TSX
+   */
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">Liverr Graphics & Design </span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Liverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="number" placeholder="min" />
+            <input type="number" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort by</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cards">
+          {gigsData.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Gigs;
